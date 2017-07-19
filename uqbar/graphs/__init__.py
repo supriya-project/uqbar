d3fd2040933f4e99be72fc0c4cf6727c9e8b850d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-07-18 22:02:29 -0700

    Fix imports and adjust Travis settings.

--- uqbar/graphs/__init__.py
+++ uqbar/graphs/__init__.py
@@ -1,8 +1,3 @@
 from uqbar.graphs.Point import Point  # noqa
 from uqbar.graphs.Color import Color  # noqa
-
 from uqbar.graphs.Attributes import Attributes  # noqa
-from uqbar.graphs.ClusterAttributes import ClusterAttributes  # noqa
-from uqbar.graphs.EdgeAttributes import EdgeAttributes  # noqa
-from uqbar.graphs.GraphAttributes import GraphAttributes  # noqa
-from uqbar.graphs.NodeAttributes import NodeAttributes  # noqa
