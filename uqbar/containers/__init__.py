8a87f5163859fd774c4d16f8088a56615ca35a9e
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-12-31 13:17:36 -0500

    Ported DependencyGraph.

--- uqbar/containers/__init__.py
+++ uqbar/containers/__init__.py
@@ -2,5 +2,6 @@
 Specialized container classes.
 """
 
+from .DependencyGraph import DependencyGraph  # noqa
 from .UniqueTreeNode import UniqueTreeNode  # noqa
 from .UniqueTreeContainer import UniqueTreeContainer  # noqa
