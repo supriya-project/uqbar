4758f4e40c56f1b0877d1c79038c72166a710991
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-04-09 16:28:25 -0400

    Add uqbar.io.find_executable(). (#11)
    * Add uqbar.io.find_executable().
    
    * Bump version.
    
    * Update tests.
    
    * Update Makefile.

--- uqbar/io/__init__.py
+++ uqbar/io/__init__.py
@@ -3,13 +3,118 @@ Tools for IO and file-system manipulation.
 """
 
 import collections
+import os
 import pathlib
-from typing import Generator, Optional, Sequence, Tuple, Union
+from typing import Generator, List, Optional, Sequence, Tuple, Union
 
 from .DirectoryChange import DirectoryChange  # noqa
 from .Profiler import Profiler  # noqa
 from .RedirectedStreams import RedirectedStreams  # noqa
 from .Timer import Timer  # noqa
+
+
+def find_common_prefix(
+    paths: Sequence[Union[str, pathlib.Path]]
+    ) -> Optional[pathlib.Path]:
+    """
+    Find the common prefix of two or more paths.
+
+    ::
+
+        >>> one = pathlib.Path('foo/bar/baz')
+        >>> two = pathlib.Path('foo/quux/biz')
+        >>> three = pathlib.Path('foo/quux/wuux')
+
+    ::
+
+        >>> import uqbar.io
+        >>> str(uqbar.io.find_common_prefix([one, two, three]))
+        'foo'
+
+    :param paths: paths to inspect
+    """
+    counter: collections.Counter = collections.Counter()
+    for path in paths:
+        path = pathlib.Path(path)
+        counter.update([path])
+        counter.update(path.parents)
+    valid_paths = sorted([
+        path for path, count in counter.items()
+        if count >= len(paths)
+        ], key=lambda x: len(x.parts))
+    if valid_paths:
+        return valid_paths[-1]
+    return None
+
+
+def find_executable(name: str, flags=os.X_OK) -> List[str]:
+    r'''Finds executable `name`.
+
+    Similar to Unix ``which`` command.
+
+    Returns list of zero or more full paths to `name`.
+    '''
+    result = []
+    extensions = [
+        x
+        for x in os.environ.get('PATHEXT', '').split(os.pathsep)
+        if x
+        ]
+    path = os.environ.get('PATH', None)
+    if path is None:
+        return []
+    for path in os.environ.get('PATH', '').split(os.pathsep):
+        path = os.path.join(path, name)
+        if os.access(path, flags):
+            result.append(path)
+        for extension in extensions:
+            path_extension = path + extension
+            if os.access(path_extension, flags):
+                result.append(path_extension)
+    return result
+
+
+def relative_to(
+    source_path: Union[str, pathlib.Path],
+    target_path: Union[str, pathlib.Path],
+    ) -> pathlib.Path:
+    """
+    Generates relative path from ``source_path`` to ``target_path``.
+
+    Handles the case of paths without a common prefix.
+
+    ::
+
+        >>> source = pathlib.Path('foo/bar/baz')
+        >>> target = pathlib.Path('foo/quux/biz')
+
+    ::
+
+        >>> target.relative_to(source)
+        Traceback (most recent call last):
+        ...
+        ValueError: 'foo/quux/biz' does not start with 'foo/bar/baz'
+
+    ::
+
+        >>> import uqbar.io
+        >>> str(uqbar.io.relative_to(source, target))
+        '../../quux/biz'
+
+    :param source_path: the source path
+    :param target_path: the target path
+    """
+    source_path = pathlib.Path(source_path).absolute()
+    if source_path.is_file():
+        source_path = source_path.parent
+    target_path = pathlib.Path(target_path).absolute()
+    common_prefix = find_common_prefix([source_path, target_path])
+    if not common_prefix:
+        raise ValueError('No common prefix')
+    source_path = source_path.relative_to(common_prefix)
+    target_path = target_path.relative_to(common_prefix)
+    result = pathlib.Path(*['..'] * len(source_path.parts))
+    return result / target_path
 
 
 def walk(
@@ -81,80 +186,3 @@ def write(
         if verbose:
             print('wrote: {}'.format(path))
     return True
-
-
-def find_common_prefix(
-    paths: Sequence[Union[str, pathlib.Path]]
-    ) -> Optional[pathlib.Path]:
-    """
-    Find the common prefix of two or more paths.
-
-    ::
-
-        >>> one = pathlib.Path('foo/bar/baz')
-        >>> two = pathlib.Path('foo/quux/biz')
-        >>> three = pathlib.Path('foo/quux/wuux')
-
-    ::
-
-        >>> import uqbar.io
-        >>> str(uqbar.io.find_common_prefix([one, two, three]))
-        'foo'
-
-    :param paths: paths to inspect
-    """
-    counter: collections.Counter = collections.Counter()
-    for path in paths:
-        path = pathlib.Path(path)
-        counter.update([path])
-        counter.update(path.parents)
-    valid_paths = sorted([
-        path for path, count in counter.items()
-        if count >= len(paths)
-        ], key=lambda x: len(x.parts))
-    if valid_paths:
-        return valid_paths[-1]
-    return None
-
-
-def relative_to(
-    source_path: Union[str, pathlib.Path],
-    target_path: Union[str, pathlib.Path],
-    ) -> pathlib.Path:
-    """
-    Generates relative path from ``source_path`` to ``target_path``.
-
-    Handles the case of paths without a common prefix.
-
-    ::
-
-        >>> source = pathlib.Path('foo/bar/baz')
-        >>> target = pathlib.Path('foo/quux/biz')
-
-    ::
-
-        >>> target.relative_to(source)
-        Traceback (most recent call last):
-        ...
-        ValueError: 'foo/quux/biz' does not start with 'foo/bar/baz'
-
-    ::
-
-        >>> import uqbar.io
-        >>> str(uqbar.io.relative_to(source, target))
-        '../../quux/biz'
-
-    :param source_path: the source path
-    :param target_path: the target path
-    """
-    source_path = pathlib.Path(source_path).absolute()
-    if source_path.is_file():
-        source_path = source_path.parent
-    target_path = pathlib.Path(target_path).absolute()
-    common_prefix = find_common_prefix([source_path, target_path])
-    if not common_prefix:
-        raise ValueError('No common prefix')
-    source_path = source_path.relative_to(common_prefix)
-    target_path = target_path.relative_to(common_prefix)
-    result = pathlib.Path(*['..'] * len(source_path.parts))
-    return result / target_path
