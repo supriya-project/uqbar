7424911d4ea5750d0c65ca159500f142d23a236d
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2022-08-14 10:55:02 -0400

    Hardening find_executable

--- tests/test_io_find_executable.py
+++ tests/test_io_find_executable.py
@@ -1,8 +1,14 @@
+import platform
 import sys
 
 import uqbar.io
 
 
 def test_find_executable():
-    found = uqbar.io.find_executable("python")
+    file_name = "python"
+    if platform.system() == "Windows":
+        file_name = "python.exe"
+    found = uqbar.io.find_executable(file_name)
+    print(found)
+    print(sys.executable)
     assert any(sys.executable.startswith(_) for _ in found)
