52fd16af7f1c02aa6c9528987b4a0332b83ac2ec
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2022-08-14 10:59:06 -0400

    Cleanup

--- tests/test_io_find_executable.py
+++ tests/test_io_find_executable.py
@@ -9,6 +9,4 @@ def test_find_executable():
     if platform.system() == "Windows":
         file_name = "python.exe"
     found = uqbar.io.find_executable(file_name)
-    print(found)
-    print(sys.executable)
     assert any(sys.executable.startswith(_) for _ in found)
