19f7e693719564433360a0c2564afddcdfbdbe10
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2022-12-30 11:34:13 -0500

    Fixup tests

--- tests/test_sphinx_inheritance.py
+++ tests/test_sphinx_inheritance.py
@@ -19,7 +19,4 @@ def test_sphinx_style_latex(app, status, warning):
     app.build()
     assert "build succeeded" in status.getvalue()
     warnings = [line.strip() for line in warning.getvalue().splitlines()]
-    if sys.version_info.minor < 11:
-        assert not warnings
-    else:
-        assert len(warnings) == 2
+    assert not warnings
