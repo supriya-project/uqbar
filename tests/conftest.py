d26859d9a904f56d988189f2c80ac15dba9e28fb
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2024-02-14 08:35:05 -0500

    Use pathlib.Path, not sphinx.testing.path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
-import os
-import pathlib
 import shutil
+from pathlib import Path
 
 import pytest
-from sphinx.testing.path import path
 
 pytest_plugins = "sphinx.testing.fixtures"
 
@@ -15,16 +13,14 @@ collect_ignore = ["roots"]
 def remove_sphinx_projects(sphinx_test_tempdir):
     # Even upon exception, remove any directory from temp area
     # which looks like a Sphinx project. This ONLY runs once.
-    roots_path = pathlib.Path(sphinx_test_tempdir)
-    for d in roots_path.iterdir():
-        if d.is_dir():
-            if pathlib.Path(d, "_build").exists():
+    roots_path = Path(sphinx_test_tempdir)
+    for path in roots_path.iterdir():
+        if path.is_dir():
+            if (Path(path) / "_build").exists():
                 # This directory is a Sphinx project, remove it
-                shutil.rmtree(str(d))
-    yield
+                shutil.rmtree(path)
 
 
 @pytest.fixture()
 def rootdir(remove_sphinx_projects):
-    roots = path(os.path.dirname(__file__) or ".").abspath() / "roots"
-    yield roots
+    yield Path(__file__).parent / "roots"
