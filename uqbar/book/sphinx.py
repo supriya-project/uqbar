c9e4c97b4863a9c5890af950a2a2e0d193614014
Joséphine Wolf Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2025-07-21 11:53:58 -0400

    Typing uqbar.book

--- uqbar/book/sphinx.py
+++ uqbar/book/sphinx.py
@@ -1,4 +1,3 @@
-import collections
 import contextlib
 import importlib
 import inspect
@@ -7,24 +6,27 @@ import logging
 import pickle
 import sqlite3
 import traceback
-from typing import Any, Dict
+from pathlib import Path
+from typing import Any, Dict, Generator
 
 from docutils.frontend import get_default_settings
-from docutils.nodes import Element, General, doctest_block, literal_block
+from docutils.nodes import Element, General, doctest_block, document, literal_block
 from docutils.parsers.rst import Directive, Parser
-from docutils.parsers.rst.directives import flag, register_directive
+from docutils.parsers.rst.directives import flag
 from docutils.utils import new_document
+from sphinx.addnodes import desc_signature
 from sphinx.util.nodes import set_source_info
 from typing_extensions import ClassVar
 
 from .console import Console, ConsoleError, ConsoleInput, ConsoleOutput
+from .extensions import Extension
 
 logger = logging.getLogger(__name__)
 
 try:
     import black
 
-    def black_format(lines):
+    def black_format(lines: list[str]) -> list[str]:
         mode = black.FileMode(
             line_length=80, target_versions=set([black.TargetVersion.PY310])
         )
@@ -32,8 +34,16 @@ try:
 
 except ImportError:
 
-    def black_format(lines):
+    def black_format(lines: list[str]) -> list[str]:
         return lines
+
+
+class uqbar_book_defaults_block(General, Element):
+    __documentation_ignore_inherited__ = True
+
+
+class uqbar_book_import_block(General, Element):
+    __documentation_ignore_inherited__ = True
 
 
 class UqbarBookDirective(Directive):
@@ -45,7 +55,7 @@ class UqbarBookDirective(Directive):
     final_argument_whitespace = True
     option_spec: ClassVar[Dict[str, Any]] = {"allow-exceptions": flag, "hide": flag}
 
-    def run(self):
+    def run(self) -> list[literal_block]:
         self.assert_has_content()
         code = "\n".join(self.content)
         block = literal_block(code, code)
@@ -69,7 +79,7 @@ class UqbarBookDefaultsDirective(Directive):
     optional_arguments = 0
     option_spec: ClassVar[Dict[str, Any]] = {}
 
-    def run(self):
+    def run(self) -> list[uqbar_book_defaults_block]:
         block = uqbar_book_defaults_block()
         for key, value in self.options.items():
             block[key] = value
@@ -84,7 +94,7 @@ class UqbarBookImportDirective(Directive):
     optional_arguments = 0
     option_spec = {"hide": flag}
 
-    def run(self):
+    def run(self) -> list[uqbar_book_import_block]:
         block = uqbar_book_import_block()
         block["path"] = self.arguments[0]
         block.line = self.content_offset  # set the content line number
@@ -92,14 +102,6 @@ class UqbarBookImportDirective(Directive):
         if "hide" in self.options:
             block["hide"] = True
         return [block]
-
-
-class uqbar_book_defaults_block(General, Element):
-    __documentation_ignore_inherited__ = True
-
-
-class uqbar_book_import_block(General, Element):
-    __documentation_ignore_inherited__ = True
 
 
 def collect_literal_blocks(document):
@@ -121,14 +123,14 @@ def collect_literal_blocks(document):
     return blocks
 
 
-def create_cache_db(path):
+def create_cache_db(path: Path | str) -> sqlite3.Connection:
     connection = sqlite3.connect(str(path))
     connection.execute("CREATE TABLE cache (path TEXT, data BLOB, hits NUMBER);")
     connection.commit()
     return connection
 
 
-def query_cache_db(connection, cache_path):
+def query_cache_db(connection: sqlite3.Connection, cache_path: str):
     result = None
     for row in connection.execute(
         "SELECT data, hits FROM cache WHERE path = ?", (cache_path,)
@@ -143,7 +145,7 @@ def query_cache_db(connection, cache_path):
     return pickle.loads(result)
 
 
-def update_cache_db(connection, cache_path, data):
+def update_cache_db(connection: sqlite3.Connection, cache_path: str, data):
     connection.execute(
         "INSERT INTO cache VALUES (?, ?, 0)", (cache_path, pickle.dumps(data))
     )
@@ -167,7 +169,9 @@ def group_literal_blocks_by_cache_path(blocks):
     return cache_mapping
 
 
-def find_traceback(console_output):
+def find_traceback(
+    console_output: list[ConsoleInput | ConsoleOutput | Extension],
+) -> str:
     for item in reversed(console_output):
         if not isinstance(item, ConsoleOutput):
             continue
@@ -179,12 +183,13 @@ def find_traceback(console_output):
             lines.pop(0)
         if lines:
             return "\n".join(lines) + "\n"
+    return ""
 
 
 @contextlib.contextmanager
 def console_context(
     *, extensions, namespace=None, setup_lines=None, teardown_lines=None, document=None
-):
+) -> Generator[Console, None, None]:
     console = Console(extensions=extensions, namespace=namespace)
     console_output, errored = console.interpret(setup_lines or [])
     if errored:
@@ -199,7 +204,13 @@ def console_context(
 
 
 def interpret_code_blocks(
-    blocks,
+    blocks: list[
+        doctest_block
+        | literal_block
+        | uqbar_book_defaults_block
+        | uqbar_book_import_block
+    ],
+    *,
     allow_exceptions=False,
     document=None,
     extensions=None,
@@ -209,7 +220,7 @@ def interpret_code_blocks(
     logger_func=None,
     use_black=False,
 ):
-    results = collections.OrderedDict()
+    results = {}
     block = blocks[0] if blocks else None
     with console_context(
         document=document,
@@ -253,7 +264,13 @@ def interpret_code_blocks(
 
 
 def interpret_code_blocks_with_cache(
-    blocks,
+    blocks: list[
+        doctest_block
+        | literal_block
+        | uqbar_book_defaults_block
+        | uqbar_book_import_block
+    ],
+    *,
     cache_path,
     connection,
     allow_exceptions=False,
@@ -266,7 +283,7 @@ def interpret_code_blocks_with_cache(
 ):
     cache_data = query_cache_db(connection, cache_path)
     if cache_data is not None:
-        local_node_mapping = collections.OrderedDict(zip(blocks, cache_data))
+        local_node_mapping = dict(zip(blocks, cache_data))
     else:
         local_node_mapping = interpret_code_blocks(
             blocks,
@@ -284,24 +301,29 @@ def interpret_code_blocks_with_cache(
     return local_node_mapping
 
 
-def interpret_import_block(console, block):
+def interpret_import_block(
+    console: Console, block: uqbar_book_import_block
+) -> tuple[list[ConsoleInput | ConsoleOutput | Extension], bool, bool]:
     code_address = block["path"]
     module_name, sep, attr_name = code_address.rpartition(":")
     module = importlib.import_module(module_name)
     attr = getattr(module, attr_name)
     source = inspect.getsource(attr)
     lines = [f"from {module_name} import {attr_name}"]
-    _, errored = console.interpret(lines)
+    output, errored = console.interpret(lines)
     output = [ConsoleOutput(string=source)]
     return output, errored, False
 
 
-def interpret_literal_block(console, block, use_black=False):
+def interpret_literal_block(
+    console: Console, block: doctest_block | literal_block, use_black: bool = False
+) -> tuple[list[ConsoleInput | ConsoleOutput | Extension], bool, bool]:
     has_exception = False
     lines = []
     contents = block[0]
-    if isinstance(contents, literal_block):
+    if isinstance(contents, (doctest_block, literal_block)):
         contents = contents[0]
+    assert isinstance(contents, str)
     for line in contents.splitlines():
         if line.startswith((">>> ", "... ")):
             lines.append(line[4:])
@@ -318,7 +340,7 @@ def interpret_literal_block(console, block, use_black=False):
     return console_output, errored, has_exception
 
 
-def literal_block_to_cache_path(block):
+def literal_block_to_cache_path(block: literal_block) -> str | None:
     desc = None
     parent = block.parent
     while desc is None and parent is not None:
@@ -329,10 +351,11 @@ def literal_block_to_cache_path(block):
             parent = parent.parent
     if desc is None:
         return None
-    desc_signature = desc[0]
-    module_path = desc_signature.attributes["module"]
-    object_path = desc_signature.attributes["fullname"]
-    id_path = desc_signature.attributes["ids"][0]
+    desc_signature_node = desc[0]
+    assert isinstance(desc_signature_node, desc_signature)
+    module_path = desc_signature_node.attributes["module"]
+    object_path = desc_signature_node.attributes["fullname"]
+    id_path = desc_signature_node.attributes["ids"][0]
     module = importlib.import_module(module_path)
     outer = inner = module
     for path in object_path.split("."):
@@ -357,10 +380,10 @@ def literal_block_to_cache_path(block):
     return id_path
 
 
-def parse_rst(rst_string):
+def parse_rst(rst_string: str) -> document:
     parser = Parser()
-    for name, class_ in []:  # Add custom directives here
-        register_directive(name, class_)
+    # for name, class_ in []:  # Add custom directives here
+    #     register_directive(name, class_)
     settings = get_default_settings(Parser)
     document = new_document("test", settings)
     parser.parse(rst_string, document)
@@ -368,7 +391,7 @@ def parse_rst(rst_string):
     return document
 
 
-def rebuild_document(document, node_mapping):
+def rebuild_document(document: document, node_mapping) -> None:
     for old_node, replacements in reversed(tuple(node_mapping.items())):
         new_nodes = []
         for is_vanilla, grouper in itertools.groupby(
